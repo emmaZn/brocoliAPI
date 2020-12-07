f0bd309622ecc887acbaf7b27c074911dc9fd44d
Emmanuelle Zenou <emmanuelle.zenou.rogala@gmail.com>
2020-12-08 00:02:05 +0100

    init commit3

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ require("./routes/notification.routes.js")(app);
 
 
 
-app.listen(5000, () => {
-    console.log(`app is running on port ${5000}`)
+app.listen('5000', () => {
+    console.log(`app is running on port ${'5000'}`)
 })
